1a33a1d8fb2650bbab905d0934447f09e9718055
Arshit_Mangukiya <63718078+Arshit-0501@users.noreply.github.com>
2021-05-03 19:37:39 +0530

    adding new things

--- Project_Work/Database/main/views.py
+++ Project_Work/Database/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Product_Database
+from .models import Product_Database, dummy
 import requests
 from .forms import UserSignUpForm,Search
 from django.contrib.auth.forms import AuthenticationForm
@@ -8,6 +8,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from selenium import webdriver as wb
 from bs4 import BeautifulSoup
+
 
 def home(request):
   if request.method=='POST':
@@ -51,118 +52,165 @@ def signup(request):
     return render(request,'main/signup.html', { 'form': fm })
 
 def logout(request):
-    g(request)
-    return redirect('home')
-
-def product(request,product):
-  form={}
-  def amazon(search):
-      #print('-----------searching in amazon-----------')
-      product_name = search.replace(" ","+")
-      amazon_string = f'https://www.amazon.in/s?k={product_name}'
-      driver.get(amazon_string)
-      soup = BeautifulSoup(driver.page_source,'html.parser')
-      results = soup.find_all('div',{'data-component-type':'s-search-result'})
-      #print('total results: ',len(results))
-      price_list = []
-      url = []
-      description_list = []
-      image_url = []
-      for item in results:
-          try:
-              image_object = item.find('img')
-              image = image_object.get('src')
-              image_url.append(image)
-              atag = item.h2.a
-              description = atag.text.strip()
-              description_list.append(description)
-              redirection_URL = 'https://www.amazon.in' + atag.get('href')
-              url.append(redirection_URL)
-              price_parent_class = item.find('span','a-price')
-              amazon_price = price_parent_class.find('span','a-offscreen').text
-              price = int(''.join(filter(lambda i: i.isdigit(), amazon_price)))
-              price_list.append(price)
-          except:
-              continue
-      min_price = min(price_list)
-      print('Description: ',description_list[price_list.index(min_price)])
-      print('Price on amazon: \u20B9',min_price)
-      print('Link:', url[price_list.index(min_price)])
-      print('Imaga URL: ',image_url[price_list.index(min_price)] )    
-          
-
-  def croma(search):
-      #print('-----------searching in croma-----------')
-      product_name = search.replace(" ","+")
-      croma_string = f'https://croma.com/search/?text={product_name}'
-      driver.get(croma_string)
-      #time.sleep(10)
-      soup = BeautifulSoup(driver.page_source,'html.parser')
-      results = soup.find_all('li',{'class':'product-item'})
-      #time.sleep(20)
-      #print('total results: ', len(results))
-      price_list = []
-      url = []
-      description_list = []
-      #image_url = []
-      for item in results:
-          try:
-              '''image_object = item.find('img')
-              image = image_object.get('src')
-              image_url.append(image)'''
-              atag = item.h3.a
-              description = atag.text.strip()
-              description_list.append(description)
-              redirection_URL = 'https://www.croma.com' + atag.get('href')
-              url.append(redirection_URL)
-              croma_price = item.find('span',{'data-testid':'price'}).text
-              price = (int(''.join(filter(lambda i: i.isdigit(), croma_price))))/100
-              price_list.append(price)
-          except:
-              continue
-      min_price = min(price_list)
-      #print('Description: ',description_list[price_list.index(min_price)])
-      #print('Link: ', url[price_list.index(min_price)])
-      #print('Price on croma: \u20B9', min_price)
-      #print('Imaga URL: ',image_url[price_list.index(min_price)] )
+  g(request)
+  return redirect('home')
 
 
-  def flipkart(search):
-      #print('-----------searching in flipkart-----------')
-      product_name = search.replace(" ","+")
-      flipkart_string = f'https://www.flipkart.com/search?q={product_name}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off'
-      driver.get(flipkart_string)
-      soup = BeautifulSoup(driver.page_source,'html.parser')
-      results = soup.find_all('div',{'class':'_1AtVbE col-12-12'})
-      #print('total results: ',len(results))
-      price_list = []
-      url = []
-      description_list = []
-      for item in results:
-          try:
-              atag = item.a
-              div_des = atag.find('div',{'class':'_4rR01T'})
-              description = div_des.text.strip()
-              description_list.append(description)
-              redirection_URL = 'https://www.flipkart.com'+ atag.get('href')
-              url.append(redirection_URL)
-              flipkart_price = atag.find('div',{'class':'_30jeq3 _1_WHN1'}).text
-              price = int(''.join(filter(lambda i: i.isdigit(), flipkart_price)))
-              price_list.append(price)
-          except:
-              continue
-      min_price = min(price_list)
-      #print('Description: ',description_list[price_list.index(min_price)])
-      #print('Price on flipkart: \u20B9',min_price)
-      #print('Link:', url[price_list.index(min_price)])
 
-  driver = wb.Chrome('F:/chromedriver/chromedriver.exe')
-  amazon(product)
-  croma(product)
-  flipkart(product)
-  if request.method=='POST':
+def product(request,product):  
+  if request.method=='GET':
+    driver = wb.Chrome('F:/chromedriver/chromedriver.exe')
+    Pname=product
+    # print('-----------searching in amazon-----------')
+    product_name = Pname.replace(" ","+")
+    amazon_string = f'https://www.amazon.in/s?k={product_name}'
+    driver.get(amazon_string)
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    results = soup.find_all('div',{'data-component-type':'s-search-result'})
+    #print('total results: ',len(results))
+    price_list = []
+    url = []
+    description_list = []
+    image_url = []
+    for item in results:
+      try:
+        image_object = item.find('img')
+        image = image_object.get('src')
+        image_url.append(image)
+        atag = item.h2.a
+        description = atag.text.strip()
+        description_list.append(description)
+        redirection_URL = 'https://www.amazon.in' + atag.get('href')
+        url.append(redirection_URL)
+        price_parent_class = item.find('span','a-price')
+        amazon_price = price_parent_class.find('span','a-offscreen').text
+        price = int(''.join(filter(lambda i: i.isdigit(), amazon_price)))
+        price_list.append(price)
+        break
+      except:
+        continue
+    min_price = min(price_list)
+    amazonP=min_price
+    linkA=url[price_list.index(min_price)]
+    imag_url= image_url[price_list.index(min_price)]
+
+  # flipkart
+    flipkart_string = f'https://www.flipkart.com/search?q={product_name}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off'
+    driver.get(flipkart_string)
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    results = soup.find_all('div',{'class':'_1AtVbE col-12-12'})
+    #print('total results: ',len(results))
+    price_list = []
+    url = []
+    description_list = []
+    for item in results:
+      try:
+        atag = item.a
+        div_des = atag.find('div',{'class':'_4rR01T'})
+        description = div_des.text.strip()
+        description_list.append(description)
+        redirection_URL = 'https://www.flipkart.com'+ atag.get('href')
+        url.append(redirection_URL)
+        flipkart_price = atag.find('div',{'class':'_30jeq3 _1_WHN1'}).text
+        price = int(''.join(filter(lambda i: i.isdigit(), flipkart_price)))
+        price_list.append(price)
+        break
+      except:
+        continue
+        
+    min_price = min(price_list)
+    flipkartP=min_price
+    linkF= url[price_list.index(min_price)]
+    des=description_list[price_list.index(min_price)]
+
+    #Croma
+    # print('-----------searching in croma-----------')
+    # product_name = search.replace(" ","+")
+    croma_string = f'https://croma.com/search/?text={product_name}'
+    driver.get(croma_string)
+    #time.sleep(10)
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    results = soup.find_all('li',{'class':'product-item'})
+    #time.sleep(20)
+    #print('total results: ', len(results))
+    price_list = []
+    url = []
+    description_list = []
+    #image_url = []
+    for item in results:
+      try:
+        '''image_object = item.find('img')
+        image = image_object.get('src')
+        image_url.append(image)'''
+        atag = item.h3.a
+        description = atag.text.strip()
+        description_list.append(description)
+        redirection_URL = 'https://www.croma.com' + atag.get('href')
+        url.append(redirection_URL)
+        croma_price = item.find('span',{'data-testid':'price'}).text
+        price = (int(''.join(filter(lambda i: i.isdigit(), croma_price))))/100
+        price_list.append(price)
+        break
+      except:
+        continue
+    min_price = min(price_list)
+    cromaP=min_price
+    linkC=url[price_list.index(min_price)]
+
+    driver.quit()
+
+    # print(Pname)
+    # print(amazonP)
+    # print(cromaP)
+    # print(flipkartP)
+    # print(imag_url)
+    # print(des)
+    # print(linkA)
+    # print(linkF)
+    # print(linkC)
+    fm={
+      'pname':Pname,
+      'des':des,
+      'imagurl':imag_url,
+      'linkA':linkA,
+      'linkF':linkF,
+      'linkC':linkC,
+      'priceA':amazonP,
+      'priceF':flipkartP,
+      'priceC':cromaP,
+    }
+    obj=dummy(id=1,des=des,imag_url=imag_url,linkA=linkA,linkC=linkC,linkF=linkF,amazonP=amazonP,flipkartP=flipkartP,cromaP=cromaP)
+    obj.save()
+    return render(request,'main/product.html',fm)
+  else:
     if request.user.is_authenticated==False:
-      messages.info(request,'please login first, nakar halti no tha')
+      # messages.info(request,'please login first!!')
+      return redirect('login')
     else:
-      pass
-  return render(request,'main/product.html')
+      Pname=product
+      pro=Product_Database.objects.filter(email=request.user.email,product_name=Pname)
+      if pro.exists():
+        pro.delete()
+      obj=dummy.objects.get(id=1)
+      pro1=Product_Database( email= request.user.email, name='amazon', price=obj.amazonP,product_name=Pname, link=obj.linkA)
+      pro1.save()
+      pro2=Product_Database( email= request.user.email, name='flipkart', price=obj.flipkartP,product_name=Pname, link=obj.linkF)
+      pro2.save()
+      pro3=Product_Database( email= request.user.email, name='croma', price=obj.cromaP,product_name=Pname, link=obj.linkC)
+      pro3.save()
+      fm={
+        'pname':Pname,
+        'des':obj.des,
+        'imagurl':obj.imag_url,
+        'linkA':obj.linkA,
+        'linkF':obj.linkF,
+        'linkC':obj.linkC,
+        'priceA':obj.amazonP,
+        'priceF': obj.flipkartP,
+        'priceC': obj.cromaP,
+      }
+      messages.info(request,'success')
+      return render(request,'main/product.html',fm)
+
+
+
