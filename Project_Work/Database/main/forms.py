943ef082e2caa5376b21f6392939862d5cc463f6
Arshit_Mangukiya <63718078+Arshit-0501@users.noreply.github.com>
2021-05-04 21:47:06 +0530

    Ading change password

--- Project_Work/Database/main/forms.py
+++ Project_Work/Database/main/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm ,PasswordChangeForm
 class UserSignUpForm(UserCreationForm):
     email=forms.EmailField(max_length=70)
     class Meta:
@@ -9,3 +9,9 @@ class UserSignUpForm(UserCreationForm):
 
 class Search(forms.Form):
     name=forms.CharField(widget =forms.TextInput(attrs={'placeholder': 'Search', 'class': 'form-control'}))
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['username','password']
+
